44e9866c85e450872ab24cccb69d6df510e69358
HendMohammed90 <hend.odesk.tasks@gmail.com>
2022-12-12 12:43:00 -0800

    update task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,13 @@
 
 /**
 * this represnt a test for if Statment in C
-*
 * also we will know more a bout generat random number
+*
+* main - Entry point
+*
+* Description: print value of n satus; zero, positive or negative
+*
+* Return: Always 0 (Success)
 */
 
 /* betty style doc for function main goes there */
@@ -17,10 +22,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		printf ("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else if (n == 0)
-		printf ("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	else
-		printf ("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	return (0);
 }
