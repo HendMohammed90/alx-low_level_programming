be9fe559545b3fc23c4b3cfddd6feebd01027353
HendMohammed90 <hend.odesk.tasks@gmail.com>
2022-12-12 12:48:42 -0800

    last time -_-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/**
-* 
+/** 
 * main - Entry point
 *
 * Description: print value of n satus; zero, positive or negative
@@ -12,7 +10,6 @@
 * Return: Always 0 (Success)
 */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
