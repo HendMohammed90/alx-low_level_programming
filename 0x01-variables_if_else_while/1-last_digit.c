c6d7fd3c7baa89c96f4a7bd93abe1396850d5963
HendMohammed90 <hend.odesk.tasks@gmail.com>
2022-12-12 22:36:47 -0800

    ubdate on task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,18 +3,19 @@
 #include <stdio.h>
 
 /**
-*
 * main - Entry point
 *
-* Description: print the value of n status greater than, is zero and is not less than 6.
+* Description: print the value of n status greater than, is zero and is not less than 6
 *
 * Return: Always 0 (Success)
 *
 */
+
 int main(void)
 {
 	int n, digit;
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10; /*gets last digit*/
 	if (digit > 5)
